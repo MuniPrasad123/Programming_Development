f8c9ad6e3de8ca001f4c2ccbf3494fe45bcd3860
muniprasad <prasad.krish95@gmail.com>
2020-07-23 09:07:02 -0700

    First Java Spring Project

---
+++ first_java_proj/src/CreativeWorld/HelloWorld.java
@@ -0,0 +1,13 @@
+package CreativeWorld;
+
+public class HelloWorld {
+	private String message = "test";
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public void getMessage() {
+		System.out.println("Your Message : " + message);
+	}
+}
